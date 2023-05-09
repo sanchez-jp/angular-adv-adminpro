13785a1f8bace171a07f5b80569fb264e56caa45
Juampe Sánchez <juanpedro.sanchez@preving.com>
2023-05-09 10:24:28 +0200

    Se implementan rutas hijas para el componente pages, se crea un archivo pages.routing.ts en la carpeta pages, se importa el componente pages y se crean las rutas hijas para el componente pages, se importa el archivo pages.routing.ts en el archivo app-routing.module.ts y se agregan las rutas hijas para el componente pages. ```

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,27 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { PagesRoutingModule } from './pages/pages.routing';
+
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
 import { NopagefoundComponent } from './nopagefound/nopagefound.component';
-import { PagesComponent } from './pages/pages.component';
+
 
 const routes: Routes = [
-  // Rutas protegidas
-  { 
-    path: '', 
-    component: PagesComponent,
-    children: [
-      // Todas estas rutas cumplen las mismas condiciones de diseño
-      { path: 'dashboard', component: DashboardComponent },
-      { path: 'progress', component: ProgressComponent},
-      { path: 'grafica1', component: Grafica1Component },
-      { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
-    ]
-  },
-
   // Rutas públicas
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
@@ -33,7 +19,8 @@ const routes: Routes = [
 @NgModule({
   declarations: [],
   imports: [
-    RouterModule.forRoot(routes) // se importa el modulo de rutas con las rutas principales
+    RouterModule.forRoot(routes), // se importa el modulo de rutas con las rutas principales
+    PagesRoutingModule
   ],
   exports: [RouterModule]
 })
