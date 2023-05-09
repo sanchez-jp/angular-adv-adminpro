973bae09f3a8c710c70d8444a6250e2a668eca70
Juampe Sánchez <juanpedro.sanchez@preving.com>
2023-05-09 10:14:39 +0200

    Reorganización de código. Creación de módulo para auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { ProgressComponent } from './pages/progress/progress.component';
 import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 import { PagesComponent } from './pages/pages.component';
 
 const routes: Routes = [
@@ -27,7 +27,6 @@ const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   
   // Ruta por defecto
-
   { path: '**', component: NopagefoundComponent }
 ];
 
