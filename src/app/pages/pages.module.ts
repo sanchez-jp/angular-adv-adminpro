7496ebdf5554eab1d574f91799670895cc387359
Juampe Sánchez <juanpedro.sanchez@preving.com>
2023-05-12 09:06:25 +0200

    Implement. loaders y contadores incrementadores

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -6,6 +6,7 @@ import { Grafica1Component } from './grafica1/grafica1.component';
 import { PagesComponent } from './pages.component';
 import { SharedModule } from '../shared/shared.module';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 
 
@@ -24,6 +25,7 @@ import { RouterModule } from '@angular/router';
   ],
   imports: [
     CommonModule,
+    FormsModule,
     SharedModule,
     RouterModule
   ]
--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -3,9 +3,35 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-progress',
   templateUrl: './progress.component.html',
-  styles: [
-  ]
+  styleUrls: ['./progress.component.css']
 })
 export class ProgressComponent {
 
+  progreso: number = 25;
+
+  getPorcentaje(): string {    
+    return `${this.progreso}%`;
+  }
+
+  cambiarValor (valor: number) {   
+    if (this.progreso >= 100 && valor > 0) {
+      this.progreso = 100;
+      return;
+    }
+    if (this.progreso <= 0 && valor < 0) {
+      this.progreso = 0;
+      return;
+    }
+    this.progreso += valor;
+  }
+
+  validarVaor (valor: number) {
+    if (this.progreso >= 100 && valor > 0) {
+      return true;
+    }
+    if (this.progreso <= 0 && valor < 0) {
+      return true;
+    }
+    return false;
+  }
 }
