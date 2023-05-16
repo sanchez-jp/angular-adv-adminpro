82dfedca390f260f626acc0e4faee59b176c58ce
Juampe Sánchez <juanpedro.sanchez@preving.com>
2023-05-16 17:16:56 +0200

    Optimización de código

--- src/app/pages/accout-settings/accout-settings.component.ts
+++ src/app/pages/accout-settings/accout-settings.component.ts
@@ -6,14 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class AccoutSettingsComponent {
+export class AccoutSettingsComponent implements OnInit {
   
   public linkTheme = document.querySelector('#theme');
+  public links!: NodeListOf<Element>; // Especifica el tipo de la propiedad links como una lista de nodos de elementos
 
-  constructor() { }
+ /* La exclamación ! después del nombre de la propiedad links es un operador de aserción de no nulidad. 
+    Esto le dice a TypeScript que la propiedad links nunca será null o undefined, incluso si no se le asigna un valor inicialmente.
+
+    Este operador se utiliza a menudo para evitar errores de comprobación de nulidad cuando se sabe que una propiedad siempre tendrá 
+    un valor en tiempo de ejecución, pero TypeScript no puede inferirlo por sí mismo. 
+    Sin embargo, debes tener cuidado al utilizar este operador, ya que si la propiedad en realidad puede ser null o undefined 
+    en tiempo de ejecución, el uso del operador de aserción de no nulidad puede ocultar errores y causar problemas en tiempo de ejecución.*/
+  
+    constructor() { }
 
   ngOnInit(): void {
-    
+    this.links = document.querySelectorAll('.selector');
+    this.checkCurrentTheme();
   }
 
   changeTheme( theme: string ) {   
@@ -26,6 +36,29 @@ export class AccoutSettingsComponent {
       localStorage.setItem('theme', url);
     }
 
+    this.checkCurrentTheme();
+
   }
+
+  checkCurrentTheme(  ) {
+      
+
+      this.links.forEach( elem => {
+          
+          elem.classList.remove('working');
+  
+          const btnTheme = elem.getAttribute('data-theme');
+          const btnThemeUrl = `./assets/css/colors/${ btnTheme }.css`;
+          const currentTheme = this.linkTheme?.getAttribute('href');
+  
+          if ( btnThemeUrl === currentTheme ) {
+            elem.classList.add('working');
+          } else {
+            elem.classList.remove('working');
+          }
+          
+        });
+  
+    } 
 
 }
