e0e0e84ea6aa45057b7fd1c2486ea6663a747386
Juampe Sánchez <juanpedro.sanchez@preving.com>
2023-05-18 11:54:23 +0200

    Creación de función que retorna una promesa

--- src/app/pages/promesas/promesas.component.ts
+++ src/app/pages/promesas/promesas.component.ts
@@ -12,22 +12,35 @@ export class PromesasComponent implements OnInit {
   
   ngOnInit(): void {
     
-    const promesa = new Promise((resolve, reject) => {
-      if (false) {
-        resolve('Hola mundo');
-      } else {
-        reject('Algo salió mal');
-      }
-    });
+    // const promesa = new Promise((resolve, reject) => {
+    //   if (false) {
+    //     resolve('Hola mundo');
+    //   } else {
+    //     reject('Algo salió mal');
+    //   }
+    // });
 
-    promesa.then((mensaje) => {
-      console.log(mensaje); // Devuelve: Hola mundo (lo que hay en resolve)
-    }).catch((error) => {
-      console.log('Error en mi promesa.', error); // Devuelve: Algo salió mal (lo que hay en reject)
-    });
+    // promesa.then((mensaje) => {
+    //   console.log(mensaje); // Devuelve: Hola mundo (lo que hay en resolve)
+    // }).catch((error) => {
+    //   console.log('Error en mi promesa.', error); // Devuelve: Algo salió mal (lo que hay en reject)
+    // });
 
-    console.log('Fin del Init');
-    
+    // console.log('Fin del Init');
+    this.getUsuarios().then(usuarios => {
+      console.log(usuarios);
+    }).catch(error => {
+      console.log('Error en la promesa', error);
+    });
+  }
+
+  getUsuarios() { // Promesa
+
+    return new Promise((resolve) => {
+      fetch('https://reqres.in/api/users')
+        .then(resp => resp.json())
+        .then(body => resolve(body.data));
+    });
   }
     
 }
